c8ceb16e450912b5f88aa2660802e9d6f10101d3
Bryce Pingul <brycepingul@gmail.com>
2020-08-05 17:09:37 -0700

    fixing mLab deploy

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://user-name:Jack11dog@ds035703.mlab.com:35703/heroku_w2lv1wng", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
